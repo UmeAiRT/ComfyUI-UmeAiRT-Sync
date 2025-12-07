d044278ffbaaec2a558a78ac8b33cc351c8665d7
Samuel Moreno <contact@smoreno.me>
2025-12-07 11:22:54 +0100

    Upload init.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
+# Console confirmation message
+print("\033[34mComfyUI-UmeAiRT-Workflows: \033[92mLoaded successfully! \033[0m")
